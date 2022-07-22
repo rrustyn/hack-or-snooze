e0000e7bebee5525653cda11f2c5232d41d49cd3
Emily Chen <sticky_sugar@hotmail.com>
2022-07-22 15:42:02 -0700

    add toggle function to favorite/unfavorite story, and change html markup to include stars

--- js/stories.js
+++ js/stories.js
@@ -21,10 +21,21 @@ async function getAndShowStoriesOnStart() {
 
 function generateStoryMarkup(story) {
   // console.debug("generateStoryMarkup", story);
-
   const hostName = story.getHostName();
+
+  //if current user fav stories includes story id of story
+  //use filled star
+  //if not, use unfilled star
+
+  const star = currentUser.favorites.some(
+    favorite => favorite.storyId === story.storyId) ?
+    "bi-star-fill" : "bi-star";
+
   return $(`
       <li id="${story.storyId}">
+        <span class="star">
+          <i class="bi ${star}"></i>
+        <span>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -35,14 +46,14 @@ function generateStoryMarkup(story) {
     `);
 }
 
-/** Gets list of stories from server, generates their HTML, and puts on page. */
+/** Takes input list of stories, generates their HTML, and puts on page. */
 
 function putStoriesOnPage(list) {
   console.debug("putStoriesOnPage");
 
   $allStoriesList.empty();
 
-  // loop through all of our stories and generate HTML for them
+  // loop through our list of stories and generate HTML for them
   for (let story of list) {
     const $story = generateStoryMarkup(story);
     $allStoriesList.append($story);
@@ -69,3 +80,30 @@ async function submitNewStory(evt) {
 }
 
 $("#submit-form").on("submit", submitNewStory);
+
+
+/** Lets user favorite/unfavorite a story */
+async function toggleFavorite(evt) {
+  //listen to click on star
+  //if star is filled, remove favorite
+  //remove current class, add new empty star class
+  //if star is empy, add favorite
+  //remove current class, add new filled star class
+
+  const $star = $(evt.target);
+  const storyId = $star.closest("li").attr("id");
+  console.log(storyId, typeof storyId);
+  const targetStory = await Story.getStoryById(storyId);
+
+  $star.toggleClass("bi-star bi-star-fill");
+
+  if ($star.hasClass("bi-star-fill")) {
+    //add story to user favorites list
+    currentUser.addFavorite(targetStory);
+  } else {
+    currentUser.removeFavorite(targetStory);
+  }
+}
+
+$allStoriesList.on("click", "span", toggleFavorite);
+
